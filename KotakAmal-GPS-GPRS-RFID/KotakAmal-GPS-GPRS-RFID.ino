b85826f8fbeca8e4670f5b5346fa78293f19e0c2
Satria Wijayandaru <satriawijayandaru@gmail.com>
2023-07-03 09:53:22 +0700

    adding sim800l module, sim dected, http untested

--- KotakAmal-GPS-GPRS-RFID/KotakAmal-GPS-GPRS-RFID.ino
+++ KotakAmal-GPS-GPRS-RFID/KotakAmal-GPS-GPRS-RFID.ino
@@ -11,6 +11,11 @@
    - GPS RX       = 22
 */
 
+/*
+   ERROR CODE DECLARATION\
+   0 = WORKING FINE
+   1 = RFID MODULE NOT DETECTED
+*/
 //#include <ESP8266WiFi.h>
 //#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
@@ -22,7 +27,11 @@
 #include <MFRC522.h>
 #include <Wire.h>
 #include "ClickButton.h"
+#include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include "SIM800L.h"
+
+#define internalLed 2
 
 //WIFI AND MQTT BROKER DECLARATION
 #ifndef STASSID
@@ -49,6 +58,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 String readUIDcontent;
 bool UIDbtnStat = 0;
 String compareUID;
+bool isRC522Detected = false;
 
 //BUTTON CONFIG (ON BOARD FLASH BUTTON)
 int btn = 0;
@@ -69,24 +79,41 @@ int ledPin = LED_BUILTIN;
 int solenoidPin = 21;
 
 //SIM MODULE SIM800L DECLARATION
-//#define simSeri/al SERIAL2
+#define SIM800_RST_PIN 39             //CHANGE IF NECESSARY, THIS PIN IS INPUT ONLY
+#define RXD2 16
+#define TXD2 17
+int simBaudRate = 9600;
+SIM800L* sim800l;
 
 //GPS DECLARATION
 int gpsRX = 22;
-int gpsTX = 15;
-EspSoftwareSerial::UART gpsSerial;
+int gpsTX = 36;                       //CHANGE IF NECESSARY, THIS PIN IS INPUT ONLY
+double Lat, Lon, Alt;
+//double lat, lng, alt/;
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(gpsRX, gpsTX);
+//EspSoftwareSerial::UART gpsSerial;/
 
 //BUZZER DECLARATION
 int buzzerPin = 13;
 
+//ERROR FLAG CODE
+int errorCode = 0;
+unsigned long previousMillisErrorHandling = 0;
+const long intervalErrorHandling = 1000;
+
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(115200);
-  gpsSerial.begin(9600, SWSERIAL_8N1, gpsRX, gpsTX, false);
-  setupWifi();
+  Serial2.begin(simBaudRate, SERIAL_8N1, RXD2, TXD2);
+  gpsSerial.begin(9600);
+  //  setupWifi();/
   pinMode(btn, INPUT);
   pinMode(solenoidPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(internalLed, OUTPUT);
+
+  sim800l = new SIM800L((Stream *)&Serial2, SIM800_RST_PIN, 200, 512);
+  checkSIM();
 
   btn1.debounceTime   = 20;
   btn1.multiclickTime = 250;
@@ -94,15 +121,20 @@ void setup() {
   EEPROM.begin(512);
   SPI.begin();
   mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
+  detectRC522Module();
   compareUID = readEEPROM(rfidUID1);
   digitalWrite(solenoidPin, LOW);
-  checkGpsSerial();
+  //  checkGpsSerial();/
   startupTone();
 }
 
 void loop() {
   readRFID();
   readRFIDStandby();
+  getGPSData();
+
+  //  detectRC522Module();/
   btn1.Update();
   if (btn1.clicks != 0) btnFunc = btn1.clicks;
   if (btnFunc == -1) {
@@ -112,18 +144,108 @@ void loop() {
     //    eepromClear();/
     Serial.print("EEPROM VALUE BEFORE = ");
     Serial.println(readEEPROM(rfidUID1));
-
+  }
+  unsigned long currentMillisErrorHandling = millis();
+  if (currentMillisErrorHandling - previousMillisErrorHandling >= intervalErrorHandling) {
+    previousMillisErrorHandling = currentMillisErrorHandling;
+    //    mfrc522.PCD_DumpVersionToSerial(); /
+    //    errorChecking();/
   }
 }
 
-void checkGpsSerial() {
-  if (!gpsSerial) { // If the object did not initialize, then its configuration is invalid
-    Serial.println("Invalid EspSoftwareSerial pin configuration, check config");
-    while (1) { // Don't continue with invalid configuration
-      delay (1000);
+void errorChecking() {
+  Serial.print("RFID STATUS : ");
+  Serial.println(detectRC522Module());
+}
+
+void checkSIM() {
+  while (!sim800l->isReady()) {
+    Serial.println(F("Problem to initialize AT command, retry in 1 sec"));
+    delay(1000);
+  }
+
+  // Active echo mode (for some module, it is required)
+  sim800l->enableEchoMode();
+
+  Serial.println("Module ready");
+
+  // Print version
+  Serial.print("Module ");
+  Serial.println(sim800l->getVersion());
+  Serial.print("Firmware ");
+  Serial.println(sim800l->getFirmware());
+
+  // Print SIM card status
+  Serial.print(F("SIM status "));
+  Serial.println(sim800l->getSimStatus());
+
+  // Print SIM card number
+  Serial.print("SIM card number ");
+  Serial.println(sim800l->getSimCardNumber());
+
+  // Wait for the GSM signal
+  uint8_t signal = sim800l->getSignal();
+  while (signal <= 0) {
+    delay(1000);
+    signal = sim800l->getSignal();
+  }
+
+  if (signal > 5) {
+    Serial.print(F("Signal OK (strenght: "));
+  } else {
+    Serial.print(F("Signal low (strenght: "));
+  }
+  Serial.print(signal);
+  Serial.println(F(")"));
+  delay(1000);
+
+  // Wait for operator network registration (national or roaming network)
+  NetworkRegistration network = sim800l->getRegistrationStatus();
+  while (network != REGISTERED_HOME && network != REGISTERED_ROAMING) {
+    delay(1000);
+    network = sim800l->getRegistrationStatus();
+  }
+  Serial.println(F("Network registration OK"));
+  sim800l->setupGPRS("internet");
+  sim800l->connectGPRS();
+
+  Serial.println("End of test protocol");
+}
+
+void getGPSData() {
+  while (gpsSerial.available() > 0) {
+    digitalWrite(internalLed, HIGH);
+    Serial.println(gps.satellites.value());
+    gps.encode(gpsSerial.read());
+    if (gps.location.isUpdated()) {
+      Lat     = gps.location.lat();    //HAVEN'T TRY THE "6-s and 2-s" PARAMETER, TRY THIS ONE IF SOMETHING WIERD HAPPENS
+      Lon     = gps.location.lng();
+      Alt     = gps.altitude.meters();
     }
+    digitalWrite(internalLed, LOW);
   }
 }
+
+
+int detectRC522Module() {
+  if (mfrc522.PCD_PerformSelfTest()) {
+    return 0;
+
+  }
+  else {
+    return 1;
+  }
+
+}
+
+//void checkGpsSerial() {
+//  if (!gpsSerial) { // If the object did not initialize, then its configuration is invalid
+//    Serial.println("Invalid EspSoftwareSerial pin configuration, check config");
+//    while (1) { // Don't continue with invalid configuration
+//      delay (1000);
+//    }
+//  }
+//}
 
 void solenoidControl(String UID) {
   if (compareUID == UID) {
